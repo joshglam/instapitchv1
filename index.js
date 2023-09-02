80457b1566341960693d946283a3d8f40973deef
josh <jglam88@gmail.com>
2023-09-02 00:25:37 -0400

    creates process to clear out tools display as other tool buttons are clicked

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const toolsDisplay = document.querySelector("#tools-display")
 const toolsEmailButton = document.querySelector("#emails")
+const toolsExplination = document.querySelector("#tools-display-explination")
+const toolsContainer = document.querySelector("#tools-container")
 
 fetch("http://localhost:3000/tools")
 .then(res => res.json())
 .then(tools => {
     const emailArray = tools[0].emails
     toolsEmailButton.addEventListener("click", e =>{
+        toolsDisplay.removeChild(toolsContainer)
         emailArray.forEach(email => {
-            console.log(email)
+            console.log("hello!")
+            const toolsContainer = document.createElement("section")
             const createDiv = document.createElement("div")
             const createType = document.createElement("p")
             const createSubject = document.createElement("p")
             const createBody = document.createElement("p")
+            toolsContainer.id = "tools-container"
             createDiv.className = "tools-email"
 
-            toolsDisplay.append(createDiv)
+            toolsDisplay.append(toolsContainer)
+            toolsContainer.append(createDiv)
 
             createType.textContent = email.type 
             createSubject.textContent = email.subject
