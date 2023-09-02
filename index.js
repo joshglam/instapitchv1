f90d52875e1434a19ede045c95fb9f8a8eb56321
josh <jglam88@gmail.com>
2023-09-02 18:33:08 -0400

    deletes tools container and replaces it with emails or button depending on users click

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ function collapseButton(button) {
     })
 }
 
+
+
+
+
+
+
 // uses fetch to display email in tools
 const toolsDisplay = document.querySelector("#tools-display")
 const toolsEmailButton = document.querySelector("#emails")
@@ -35,21 +41,25 @@ const toolsContainer = document.querySelector("#tools-container")
 fetch("http://localhost:3000/tools")
 .then(res => res.json())
 .then(tools => {
-    const emailArray = tools[0].emails
+    const emails = tools[0].emails
     toolsEmailButton.addEventListener("click", e =>{
-        toolsDisplay.removeChild(toolsContainer)
-        emailArray.forEach(email => {
+        
+        const deleteFirst = document.querySelector("#tools-container")
+        deleteFirst.remove()
+        const newToolsContainer = document.createElement("div")
+        newToolsContainer.id = "tools-container"
+        toolsDisplay.append(newToolsContainer)
+
+        emails.forEach(email => {
             console.log("hello!")
-            const toolsContainer = document.createElement("section")
             const createDiv = document.createElement("div")
             const createType = document.createElement("p")
             const createSubject = document.createElement("p")
             const createBody = document.createElement("p")
-            toolsContainer.id = "tools-container"
             createDiv.className = "tools-email"
 
-            toolsDisplay.append(toolsContainer)
-            toolsContainer.append(createDiv)
+            toolsDisplay.append(newToolsContainer)
+            newToolsContainer.append(createDiv)
 
             createType.textContent = email.type 
             createSubject.textContent = email.subject
@@ -58,5 +68,32 @@ fetch("http://localhost:3000/tools")
             createDiv.append(createType, createSubject, createBody)
         })
     })
-})
 
+    const coldCallButton = document.querySelector("#cold-call")
+    const callScript = tools[1].callScripts
+
+    coldCallButton.addEventListener("click", e =>{
+        const deleteFirst = document.querySelector("#tools-container")
+        deleteFirst.remove()
+        const newToolsContainer = document.createElement("div")
+        newToolsContainer.id = "tools-container"
+        toolsDisplay.append(newToolsContainer)
+
+        callScript.forEach(call => {
+            const toolContainer = document.createElement("div")
+            const createDiv = document.createElement("div")
+            const createType = document.createElement("p")
+            const createScript = document.createElement("p")
+            toolsContainer.id = "tools-container"
+            createDiv.className = "tools-call"
+
+            newToolsContainer.append(toolContainer)
+            toolContainer.append(createDiv)
+
+            createType.textContent = call.type 
+            createScript.textContent = call.script
+
+            createDiv.append(createType, createScript)
+        })
+    })
+})
